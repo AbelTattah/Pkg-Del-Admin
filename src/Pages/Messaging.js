705fe1ebae3c174c3868f8cbe4404243dfa2567f
hermes-4 <ebenezeroforia08@gmail.com>
2024-02-07 00:09:40 +0000

    style: Create and style the seach section

--- src/Pages/Messaging.js
+++ src/Pages/Messaging.js
@@ -1,41 +1,59 @@
-import React from 'react'
-import BottomNavbar from '../Components/Navbar'
-import '../Styling/message.css'
+import React from "react";
+import Navbar from "../Components/Navbar";
+import "../Styling/message.css";
 
-function Messaging () {
-  return (
-    <div className='MessagesM'>
-      <div className='topSection'>
-        <div className='title'>
-          <div className='title0'>
-            <text>Payload</text>
-          </div>
-          <div className='title1'>
-            <text>Help and Support</text>
-          </div>
-        </div>
-        <div id='navbar'>
-          <BottomNavbar />
-        </div>
-      </div>
-
-      <div className='MessagesMain'>
-        <div className='messagesView'>
-          <div className='listOfUsers' />
-          <div className='Message'>
-            <div className='previousMessages' />
-            <div className='sendMessage'>
-              <textarea />
+function Messaging() {
+    return (
+        <div className="MessagesM">
+            <div className="topSection">
+                <div className="title">
+                    <div className="title0">
+                        <text>Payload</text>
+                    </div>
+                    <div className="title1">
+                        <text>Help and Support</text>
+                    </div>
+                </div>
+                <div className="navbar">
+                    <Navbar />
+                </div>
             </div>
-          </div>
+
+            <div className="MessagesMain">
+                <div className="messageSearch">
+                    <div className="messageSearchText">User Search</div>
+                    <div className="messagesSearchbar">
+                        <input
+                            className="searchInput"
+                            name="searchInput"
+                            type="text"
+                        />
+                        <button className="searchButton">Search</button>
+                    </div>
+                </div>
+                <div className="messagesView">
+                    <div className="listOfUsers">
+                        {/*Use the actual test dataset of the app  */}
+                    </div>
+                    <div className="Message">
+                        {/**
+                          Use the map function to create a list that renders the messsage as a list after it has been Entered into the chat
+                          Do the left ones separately or maybe toggle between left and right.
+                      */}
+                        <div className="previousMessages"></div>
+                        <div className="sendMessage">
+                            <textarea
+                                name="messageTextarea"
+                                cols="10"
+                                rows="3"
+                                placeholder="Enter your message..."
+                            ></textarea>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className='messagesSearchbar'>
-          <text>Search for user</text>
-          <input type='text' />
-        </div>
-      </div>
-    </div>
-  )
+    );
 }
 
-export default Messaging
+export default Messaging;
--- src/index.js
+++ src/index.js
@@ -1,48 +1,48 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import './index.css'
-import Homepage from './Pages/Homepage'
-import Login from './Pages/Login'
-import Dashboard from './Pages/Dashboard'
-import HelpandSupport from './Pages/HelpandSupport'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Messaging from './Pages/Messaging'
-import Register from './Pages/Register'
-import Control from './Pages/Control'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Homepage from "./Pages/Homepage";
+import Login from "./Pages/Login";
+import Dashboard from "./Pages/Dashboard";
+import HelpandSupport from "./Pages/HelpandSupport";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Messaging from "./Pages/Messaging";
+import Register from "./Pages/Register";
+import Control from "./Pages/Control";
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Router data-test='component-app'>
-      <Switch>
-        <Route exact path='/'>
-          <Homepage />
-        </Route>
-        <Route path='/Login'>
-          <Login />
-        </Route>
-        <Route path='/Home'>
-          <Homepage />
-        </Route>
-        <Route path='/Control'>
-          <Control />
-        </Route>
-        <Route path='/Help'>
-          <HelpandSupport />
-        </Route>
-        <Route path='/Dashboard'>
-          <Dashboard />
-        </Route>
-        <Route path='/Ins'>
-          <Messaging />
-        </Route>
-        <Route path='/adreg'>
-          <Register />
-        </Route>
-      </Switch>
-    </Router>
-  </React.StrictMode>
-)
+    <React.StrictMode>
+        <Router data-test="component-app">
+            <Switch>
+                <Route exact path="/">
+                    <Homepage />
+                </Route>
+                <Route path="/Login">
+                    <Login />
+                </Route>
+                <Route path="/Home">
+                    <Homepage />
+                </Route>
+                <Route path="/Control">
+                    <Control />
+                </Route>
+                <Route path="/Help">
+                    <HelpandSupport />
+                </Route>
+                <Route path="/Dashboard">
+                    <Dashboard />
+                </Route>
+                <Route path="/Ins">
+                    <Messaging />
+                </Route>
+                <Route path="/adreg">
+                    <Register />
+                </Route>
+            </Switch>
+        </Router>
+    </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
